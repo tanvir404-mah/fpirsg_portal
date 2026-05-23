eb0689bc73bd15c5c844f7718d57d4904d297270
Tanvir Mahmud <tanvirmahmud150922@gmail.com>
2026-05-23 20:10:39 +0600

    added vercel configuration

--- fpirsg_portal/wsgi.py
+++ fpirsg_portal/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fpirsg_portal.settings')
 
 application = get_wsgi_application()
+
+# ফাইলের শেষে এটি যোগ করুন
+app = application
